0a69b576a1f23296fd70b311c574cbfc9f0e63fd
Thomas <173218@ids.upchiapas.edu.mx>
2021-08-18 22:26:26 -0500

    fixing max length of link in languages

--- languages/models.py
+++ languages/models.py
@@ -5,5 +5,5 @@ class Language(models.Model):
     language = models.CharField(max_length=30)
     teacher = models.CharField(max_length=30)
     level = models.CharField(max_length=30)
-    link = models.CharField(max_length=30)
+    link = models.CharField(max_length=100)
     usn = models.CharField(max_length=30)
