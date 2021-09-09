2685baf2da6ad45a6db4da15087533c0a2b3d72a
Thomas <173218@ids.upchiapas.edu.mx>
2021-09-09 11:52:13 -0500

    languages x2

--- languages/serializer.py
+++ languages/serializer.py
@@ -6,7 +6,7 @@ class LanguageSerializer(serializers.ModelSerializer):
     language = serializers.CharField()
     teacher = serializers.CharField()
     level = serializers.CharField()
-    link = serializers.CharField()
+#    link = serializers.CharField()
     usn = serializers.CharField()
     
     def create(self, validate_data):
@@ -14,7 +14,7 @@ class LanguageSerializer(serializers.ModelSerializer):
         instance.language = validate_data.get('language')
         instance.teacher = validate_data.get('teacher')
         instance.level = validate_data.get('level')
-        instance.link = validate_data.get('link')
+        #instance.link = validate_data.get('link')
         instance.usn = validate_data.get('usn')
         instance.save()
         return instance
